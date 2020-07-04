42851acd89d14128f895441dab657f0a887fe270
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-07-03 22:39:34 -0300

    commit inicial do projeto

---
+++ src/udemy/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package udemy;
+
+/**
+ * Comentarios sobre o programa
+ * 
+ * @author RafaelNGP<rafaelferreira.dev3@gmail.com>
+ * @version 1.0
+ *
+ */
+
+public class PrimeiroPrograma {
+		public static void main(String[] args) {
+//			uma sentenca de codigo termina em ;
+//			Mais um comentario...
+//			Fim
+			System.out.println("Hello World");
+			
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3
+		 */
+			
+			
+			System.out.println("Hello World #2"); // Aqui tbm funciona!
+			System.out.println("Fim");
+		}
+}
