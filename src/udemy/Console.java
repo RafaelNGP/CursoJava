f966a31b3a7de58356703a28089dbbffde0578af
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-07 03:46:52 -0300

    commit inicial da aula de scanner.
    Ensinou a utilizar a java.util.Scanner para registrar os dados inputados
    pelo usuario no console.

---
+++ src/udemy/Console.java
@@ -0,0 +1,38 @@
+package udemy;
+
+import java.util.Scanner;
+
+public class Console {
+	public static void main(String[] args) {
+		System.out.print("Bom");
+		System.out.print(" dia\n");
+		
+		System.out.println("Bom");
+		System.out.println("dia");
+	
+		System.out.printf("Megasena: %d %d %d %d %d %d", 
+				1, 2, 3, 4, 5, 6);
+		System.out.printf("\nSalario: %.1f", 1235.59);
+		System.out.printf("\nNome: %s", "Rafael");
+	
+		Scanner entrada = new Scanner(System.in);
+		try {	
+			System.out.print("\n\nDigite um nome: ");
+			String nome = entrada.nextLine();
+		
+			System.out.print("Digite seu sobrenome: ");
+			String sobrenome = entrada.nextLine();
+		
+			System.out.print("Qual sua idade? ");
+			int idade = entrada.nextInt();
+		
+			System.out.printf("O nome digitado foi: %s %s "
+				+ "\nE voce tem %d anos", nome, sobrenome, idade);
+			
+		} catch (Exception inputException) {
+			System.out.println("Voce digitou uma idade invalida.");
+		} finally {
+			entrada.close();
+		}
+	}
+}
