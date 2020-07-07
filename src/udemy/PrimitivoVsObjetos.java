dd30d630ca54a40c63056cbe2e154f7d633093a3
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-07 03:54:15 -0300

    commit inicial
    sem muitos detalhes, vide comentarios

---
+++ src/udemy/PrimitivoVsObjetos.java
@@ -0,0 +1,15 @@
+package udemy;
+
+public class PrimitivoVsObjetos {
+	public static void main(String[] args) {
+		String s = "Texto";
+		System.out.println(s);
+		System.out.println(s.toUpperCase());
+
+		int a = 123;
+//		a.toUpperCase(); nao existe, tipos primitivos nao possuem comportamentos.
+		
+//		Wrapper eh a versao objeto dos tipos primitivos.
+//		Abrir a aula respectiva		
+	}
+}
