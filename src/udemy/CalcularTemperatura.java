1352641fa8022fcd1269c2194846284b9183f63a
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-07 01:06:03 -0300

    reorganizacao e finally
    constantes removidas de dentro do TRY e tambem a String pedindo o valor.
    Finally criado para fechar o scanner em caso de sucesso ou falha para
    evitar resourses leak.

--- src/udemy/CalcularTemperatura.java
+++ src/udemy/CalcularTemperatura.java
@@ -3,21 +3,20 @@ import java.util.Scanner;
 
 public class CalcularTemperatura {
 	public static void main(String[] args) {//		Formula: (F-32) x 5/9 = C
+		final double ajuste = 32;
+		final double fator = 5/9.0;
+		Scanner c = new Scanner(System.in);
+		System.out.println("Insira o valor de conversao: ");
 		try {
-			Scanner c = new Scanner(System.in);
-			System.out.println("Insira o valor de conversao: ");
 			double fahrenheit = c.nextDouble();
-			System.out.println("Valor Informado: " + fahrenheit);
-
-			final double ajuste = 32;
-			final double fator = 5/9.0;
-			System.out.println("Valor ajuste: " + ajuste + ".  Valor Fator: " + fator);
-				
 			double celsius = ((fahrenheit-ajuste) * fator);
-			System.out.println("O valor em C eh: " + celsius);
-			c.close();
+			System.out.println("Valor Informado: " + fahrenheit);
+			System.out.println("Valor ajuste: " + ajuste + ".  Valor Fator: " + fator);
+			System.out.println("O valor em C eh: " + celsius);				
 		}catch (Exception inpuException) {
 			System.out.println("valor incorreto.");
-		}  
+		}finally {
+			c.close();
+		}
 	}
 }
