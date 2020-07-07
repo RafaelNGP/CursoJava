57116ddc44596ef9d945728186267927a9b9d5bb
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-07 00:50:37 -0300

    try e catch em ordem.
    reconhece se o valor for numerico ou caso uma String foi introduzida,
    gerando um tratamento para o erro.

--- src/udemy/CalcularTemperatura.java
+++ src/udemy/CalcularTemperatura.java
@@ -2,9 +2,9 @@ package udemy;
 import java.util.Scanner;
 
 public class CalcularTemperatura {
-	public static void main(String[] args) {
-try (//		Formula: (F-32) x 5/9 = C
-		Scanner c = new Scanner(System.in)) {
+	public static void main(String[] args) {//		Formula: (F-32) x 5/9 = C
+		try {
+			Scanner c = new Scanner(System.in);
 			System.out.println("Insira o valor de conversao: ");
 			double fahrenheit = c.nextDouble();
 			System.out.println("Valor Informado: " + fahrenheit);
@@ -12,11 +12,12 @@ try (//		Formula: (F-32) x 5/9 = C
 			final double ajuste = 32;
 			final double fator = 5/9.0;
 			System.out.println("Valor ajuste: " + ajuste + ".  Valor Fator: " + fator);
-			
+				
 			double celsius = ((fahrenheit-ajuste) * fator);
 			System.out.println("O valor em C eh: " + celsius);
-			
 			c.close();
-		}
+		}catch (Exception inpuException) {
+			System.out.println("valor incorreto.");
+		}  
 	}
 }
