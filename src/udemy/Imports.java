e6c2cbc6f9ef2fc2bbd6b2a8546946561aa2b420
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-07 01:16:25 -0300

    commit inicial, imports e atalho
    Ctrl + Shift + O para otimizar os imports

---
+++ src/udemy/Imports.java
@@ -0,0 +1,20 @@
+package udemy;
+
+import java.util.Date;
+
+public class Imports {
+	public static void main(String[] args) {
+//		Caminho completo, mas nao eh necessario
+		java.lang.String b = "Bom dia!";
+		java.lang.System.out.println(b);
+				
+		String s = "Bom dia!";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+//		JButton botao = new JButton();
+//		Ctrl + Shift + O otimiza os imports
+	}
+}
