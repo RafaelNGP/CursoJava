dc3c03525e2f7a200bf6818f4067faa4d10924bc
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-06 22:16:30 -0300

    commit ensinando simone

---
+++ src/udemy/NotacaoPonto.java
@@ -0,0 +1,11 @@
+package udemy;
+import java.util.Scanner;
+
+public class NotacaoPonto {
+	public static void main(String[] args) {
+		
+		Scanner c = new Scanner(System.in);
+		String nome = c.next();
+		System.out.println("O nome da pessoa eh: " + nome);
+	}
+}
