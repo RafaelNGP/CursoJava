c401918933c9f17c0953655cde95d50f3f7a6a0f
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-08 19:17:23 -0300

    commit inicial
    cast de valores.

---
+++ src/udemy/ConversaoTipoPrimitivoNumerico.java
@@ -0,0 +1,23 @@
+package udemy;
+
+public class ConversaoTipoPrimitivoNumerico {
+	public static void main(String[] args) {
+		
+//		implicito, pois um double recebeu um int, mas nao existe perda de valor.
+		double a = 1;
+		System.out.println(a);
+		
+//		explicito, pois estamos fazendo o casting do valor.
+		float b = (float) 1.0;
+		System.out.println(b);
+		
+//		explicito, mas devido a capacidade do byte vai ficar um valor truncado. 
+		int c = 130;
+		byte d = (byte) c;
+		
+//		explicito, mas existe perda de informacao.
+		double e = 1.99999;
+		int f = (int) e;
+		System.out.println(f);
+	}
+}
