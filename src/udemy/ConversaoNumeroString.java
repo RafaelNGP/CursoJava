f72adcd829518a51c042bfc146767d5345365261
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-08 19:46:36 -0300

    commit inicial, convertendo variaveis.
    utilizando funcoes do Integer para transformar o valor em String, assim
    como int com Integer.toString

---
+++ src/udemy/ConversaoNumeroString.java
@@ -0,0 +1,18 @@
+package udemy;
+
+public class ConversaoNumeroString {
+	public static void main(String[] args) {
+		
+//		Transformando o valor numerico em String.
+		Integer num1 = 10000;
+		System.out.println(num1.toString());
+		System.out.println(num1.toString().length());
+		
+		int num2 = 10000;
+		System.out.println(Integer.toString(num2));
+		System.out.println(Integer.toString(num2).length());	
+		
+		System.out.println(("" + num1).length());
+		System.out.println(("" + num2).length());
+	}
+}
