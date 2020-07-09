2dd941235130fe4c02a414f7ad68e3f9deb71a33
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-09 02:28:16 -0300

    commit inicial, desenvolvendo o tema.
    ao utilizar valores reais pode gerar um erro de codigo devido a
    conversao double > int.

---
+++ src/udemy/ConversaoStringNumero.java
@@ -0,0 +1,27 @@
+package udemy;
+
+import javax.swing.JOptionPane;
+
+public class ConversaoStringNumero {
+	public static void main(String[] args) {
+		
+//		Imputando valores.
+		String valor1 = JOptionPane.showInputDialog("Digite o primeiro numero:");
+		String valor2 = JOptionPane.showInputDialog("Digite o segundo numero:");
+		
+//		Testando que String nao soma, vai concatenar.
+		System.out.println(valor1 + valor2);
+		
+//		Convertendo String para int, usando parseInt.
+		int valor3 = Integer.parseInt(valor1);
+		int valor4 = Integer.parseInt(valor2);
+		System.out.println(valor3+valor4);		
+		
+//		Convertendo String para double, usando parseDouble.
+//		Quando precisao eh extremamente importante, importar o BigDecimal		
+		double valor5 = Double.parseDouble(valor1);
+		double valor6 = Double.parseDouble(valor2);
+		System.out.println(valor5+valor6);
+		System.out.println((valor5+valor6)/2);				
+	}
+}
