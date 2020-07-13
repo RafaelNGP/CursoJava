f817e1362c22379362baeab9e6cc3ca53e73de25
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-13 19:37:46 -0300

    commit inicial do segundo desafio do final do modulo fundamentos

---
+++ src/udemy/desafios/ConceitosBasicosEx2.java
@@ -0,0 +1,17 @@
+package udemy.desafios;
+
+import java.util.Scanner;
+
+public class ConceitosBasicosEx2 {
+
+	public static void main(String[] args) {
+//		Criar um programa que leia a temperatura em Fahrenheit e converta para Celsius.
+//		formula = °C = (°F − 32) ÷ 1, 8
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Informe o valor em F: ");
+		double fah = entrada.nextDouble();
+		double formulaCelsius = (fah - 32) / 1.8;
+		System.out.println("Temperatura em celsius: " + formulaCelsius);
+	}
+}
