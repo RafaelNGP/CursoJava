22c02594e5a6b717fc75dbbf6928ac9bbacb606a
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-13 23:50:06 -0300

    commit inicial dos conceitos de if e else

--- src/udemy/controle/If.java
+++ src/udemy/controle/If.java
@@ -6,7 +6,7 @@ public class If {
 	public static void main(String[] args) {
 //		Ao usar . no console, gera um inputexception.
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.print("Informe a media: ");
 		double nota = entrada.nextDouble();
 		if (nota <= 10 && nota >= 7) {
