a812877ab6b859e4ad67ea3c193eacf2cd0760e2
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-14 01:13:28 -0300

    commit inicial da aula

---
+++ src/udemy/controle/WhileDeterminado.java
@@ -0,0 +1,13 @@
+package udemy.controle;
+
+public class WhileDeterminado {
+	public static void main(String[] args) {
+		
+//		basicamente o mesmo que um for
+		int contador = 1;
+		while (contador <= 10) {
+			System.out.printf("i = %d\n", contador);
+			contador++;
+		}
+	}
+}
