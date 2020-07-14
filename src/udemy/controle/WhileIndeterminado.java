4d2a92539b037aec2990713bbd04a02560f4d153
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-14 01:44:57 -0300

    commit inicial da aula

---
+++ src/udemy/controle/WhileIndeterminado.java
@@ -0,0 +1,15 @@
+package udemy.controle;
+
+import java.util.Scanner;
+
+public class WhileIndeterminado {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String comando = "";
+		while (!comando.equalsIgnoreCase("sair")) {
+			System.out.print("Digite um comando: ");
+			comando = entrada.nextLine();			
+		}
+		entrada.close();
+	}
+}
