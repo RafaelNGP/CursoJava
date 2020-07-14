e600bcabbc047ef339a4df5ac1c6e0941cdffeb1
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-14 17:48:53 -0300

    commit inicial da aula

---
+++ src/udemy/controle/DoWhile.java
@@ -0,0 +1,20 @@
+package udemy.controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+	public static void main(String[] args) {
+	Scanner entrada = new Scanner(System.in);
+
+	String texto = "";
+	
+	do {
+		System.out.println("Voce precisa falar as palavras magicas");
+		System.out.print("Quer sair? ");
+		texto = entrada.nextLine().trim();
+	} while (!texto.equalsIgnoreCase("por favor"));
+
+	System.out.println("Obrigado!");
+	entrada.close();
+	}
+}
