0648a7bdd8399086e77ce3001c3d7185d50c28a6
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-13 22:21:17 -0300

    alteracao nos () das formulas.

--- src/udemy/desafios/ConceitosBasicosEx6.java
+++ src/udemy/desafios/ConceitosBasicosEx6.java
@@ -1,5 +1,7 @@
 package udemy.desafios;
 
+import java.util.Scanner;
+
 public class ConceitosBasicosEx6 {
 	public static void main(String[] args) {
 //		Criar um programa que resolve equações do segundo grau (ax2 + bx + c = 0) utilizando a fórmula de Bhaskara. 
@@ -8,23 +10,28 @@ public class ConceitosBasicosEx6 {
 //		Calculando o delta.
 //		b2 - 4*a*c
 		
-		int a = 1;
-		int b = 12;
-		int c = -13;
+		Scanner entrada = new Scanner(System.in);
 		
-		int delta = (b*b) - 4*a*c;
+		System.out.println("Digite os valores da equacao de segundo grau: (a, b e c)");
+		int a = entrada.nextInt();
+		int b = entrada.nextInt();
+		int c = entrada.nextInt();
+		
+		int delta = (b*b) - (4*a*c);
 		double raizDelta = Math.sqrt(delta);
 		
 //		Calculando baskara
 //		-B +- raiz delta / 2.a
 		
-		double baskaraMais = (-b + raizDelta) / 2 * a;
-		double baskaraMenos = (-b - raizDelta) / 2 * a;
+		double baskaraMais = (-b + raizDelta) / (2 * a);
+		double baskaraMenos = (-b - raizDelta) / (2 * a);
 		
 		System.out.printf("a = %d, b = %d, c = %d\n", a, b, c);
 		System.out.println("Delta = " + delta);
 		System.out.println("Raiz Delta = " + raizDelta);
 		System.out.println("Baskara+ = " + baskaraMais);
 		System.out.println("Baskara- = " + baskaraMenos);
+		
+		entrada.close();
 	}	
 }
