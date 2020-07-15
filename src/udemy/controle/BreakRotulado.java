8ae1d1c8f704b3345155624a0894b236f5e164a7
rafaelngp <rafaelngp@RafaelNGP-Note>
2020-07-15 19:04:22 -0300

    commit inicial da aula

---
+++ src/udemy/controle/BreakRotulado.java
@@ -0,0 +1,18 @@
+package udemy.controle;
+
+public class BreakRotulado {
+	public static void main(String[] args) {
+//	EVITAR ISTO AO MAXIMO PARA EVITAR COMPLEXIDADE
+		externo: for (int i = 0; i < 3; i++) {
+			interno: for (int j = 0; j < 3; j++) {
+
+				if (i == 1) {
+					break externo;
+				}
+				System.out.printf("%d %d ", i, j);
+			}
+			System.out.println();
+		}
+		System.out.println("Fim!");
+	}
+}
