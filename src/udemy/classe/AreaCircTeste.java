19fea852b6de9f578a0bfc6c05344ecdb27031b1
rafaelngp <rafaelferreira.dev3@gmail.com>
2020-08-03 19:19:14 -0300

    commit inicial da aula

---
+++ src/udemy/classe/AreaCircTeste.java
@@ -0,0 +1,15 @@
+package udemy.classe;
+
+public class AreaCircTeste {
+	public static void main(String[] args) {
+		AreaCirc a = new AreaCirc(5.6);
+		System.out.println(a.area());
+
+		AreaCirc a2 = new AreaCirc(5.6);
+//		AreaCirc.pi = 3.14;
+		System.out.println(a2.area());
+
+		System.out.println(AreaCirc.PI);
+		System.out.println(Math.PI);
+	}
+}
