90ff37861b6ffcb670d98368b5cdc048c3b3ebc6
rafaelngp <rafaelferreira.dev3@gmail.com>
2020-08-05 20:00:38 -0300

    commit inicial do desafio

---
+++ src/udemy/desafios/DesafioPrimeiroTrauma.java
@@ -0,0 +1,10 @@
+package udemy.desafios;
+
+public class DesafioPrimeiroTrauma {
+	int a = 3; // nao pode mexer aqui, faca funcionar
+	public static void main(String[] args) {
+		
+		DesafioPrimeiroTrauma p1 = new DesafioPrimeiroTrauma();
+		System.out.println(p1.a);
+	}
+}
