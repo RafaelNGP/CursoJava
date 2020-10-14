cb8f1acc805e4aae06ccd20cbfc73352e1977630
rafaelngp <rafaelferreira.dev3@gmail.com>
2020-10-14 03:25:40 -0300

    novas condicoes, loop while, tratamento de erros, novos JOptionPane.

--- src/gama_academy/CaixasDialogo.java
+++ src/gama_academy/CaixasDialogo.java
@@ -5,12 +5,37 @@ import javax.swing.JOptionPane;
 public class CaixasDialogo {
 	public static void main(String[] args) {
 		String nome;
-		int salario;
+		String salario;
 		int resp;
 
-		nome = JOptionPane.showInputDialog(null, "Digite seu nome");
-		JOptionPane.showMessageDialog(null, "Quer dizer entao que seu nome eh " + nome + "?");
-		resp = JOptionPane.showConfirmDialog(null, "Deseja continuar a execucao do programa?");
-		JOptionPane.showMessageDialog(null, "Sua resposta " + resp);
+		try {
+			nome = JOptionPane.showInputDialog(null, "Digite seu nome");
+			while (nome.isEmpty()) {
+				JOptionPane.showMessageDialog(null, "Voce nao digitou um nome!", "Erro 1", 3);
+				nome = JOptionPane.showInputDialog(null, "Digite seu nome");
+			}
+			JOptionPane.showMessageDialog(null, "Seja bem vindo " + nome + "!");
+			resp = JOptionPane.showConfirmDialog(null, "Gostaria de atualizar seu salario?");
+			switch (resp) {
+			case 0:
+				salario = JOptionPane.showInputDialog(null, "Qual o valor?").replace(',', '.');
+				double salarioConvertido = Double.parseDouble(salario);
+				JOptionPane.showMessageDialog(null, "Salario registrado!\nValor atual: " + salarioConvertido);
+				break;
+			case 1:
+				JOptionPane.showMessageDialog(null, "Tudo bem, continue praticando para aumentar esses numeros!");
+				break;
+			case 2:
+				JOptionPane.showMessageDialog(null, "Ok, operacao cancelada.");
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Operacao abortada");
+			}
+
+		} catch (NullPointerException e) {
+			JOptionPane.showMessageDialog(null, "Valores nulos detectados. Encerrando.");
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Nao foram digitados numeros. Encerrando.");
+		}
 	}
 }
