69fdb47bfcfc495a0f9a151eae221f738ea69b4c
rafaelngp <rafaelferreira.dev3@gmail.com>
2020-10-14 02:48:44 -0300

    alteracao na condicao, tratamento de null.

--- src/udemy/proprios/PrevisaoCasal.java
+++ src/udemy/proprios/PrevisaoCasal.java
@@ -4,13 +4,17 @@ import javax.swing.JOptionPane;
 
 public class PrevisaoCasal {
 	public static void main(String[] args) {
-		String nome1 = JOptionPane.showInputDialog("Qual o nome dele?");
-		String nome2 = JOptionPane.showInputDialog("Qual o nome dela?");
-		
-		if (nome1.toLowerCase().contains("rafael") && nome2.toLowerCase().contains("beatriz")) {
-			JOptionPane.showMessageDialog(null, "Totalmente compativeis!! <3");
-		} else {
-			JOptionPane.showMessageDialog(null, "nao sao tao perfeitos quanto o rafa e a bea");
+		try {
+			String nome1 = JOptionPane.showInputDialog(null, "Qual o nome dele?", null, 3);
+			String nome2 = JOptionPane.showInputDialog(null, "Qual o nome dela?", null, 3);
+
+			if (nome1.toLowerCase().contains("rafael") && !nome2.isEmpty()) {
+				JOptionPane.showMessageDialog(null, "Totalmente compativeis!! <3");
+			} else {
+				JOptionPane.showMessageDialog(null, "Match perfeito mesmo seria o rafael e a " + nome2.toLowerCase());
+			}
+		} catch (NullPointerException e) {
+			JOptionPane.showMessageDialog(null, "Nao foram inseridos dois valores para serem comparados.", null, 0);
 		}
 	}
 }
