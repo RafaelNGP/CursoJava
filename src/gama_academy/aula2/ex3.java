858143146f0ac1a7973d071bc3373e28e8dacc31
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-10-14 21:47:27 -0300

    usando List.of

--- src/gama_academy/aula2/ex3.java
+++ src/gama_academy/aula2/ex3.java
@@ -1,5 +1,7 @@
 package gama_academy.aula2;
 
+import java.util.List;
+
 public class ex3 {
     public static void main(String[] args) {
        int[] stdin = new int[3];
@@ -7,8 +9,19 @@ public class ex3 {
        stdin[1] = 4;
        stdin[2] = 18;
 
+        var stdin2 = List.of(
+                3, 4, 18
+        );
+
         for (int j : stdin) {
             System.out.println(j);
         }
+
+        System.out.println();
+
+        for (int j : stdin2) {
+            System.out.println(j);
+        }
+
     }
 }
