67f1cd09007b6acc64f7358088f27b11ce4409f0
rafael ferreira <rafaelferreira.dev3@gmail.com>
2021-09-14 15:17:48 -0300

    adequação para a java 11

--- src/udemy/controle/SwitchComBreak.java
+++ src/udemy/controle/SwitchComBreak.java
@@ -10,14 +10,6 @@ public class SwitchComBreak {
         System.out.println("Qual a nota do aluno? ");
         int nota = entrada.nextInt();
 
-        conceito = switch (nota) {
-            case 10, 9 -> "A";
-            case 8, 7 -> "B";
-            case 6, 5 -> "C";
-            case 4, 3 -> "D";
-            case 2, 1, 0 -> "E";
-            default -> "Nao encontrado.";
-        };
 
         System.out.println("Ele esta no conceito " + conceito);
         entrada.close();
