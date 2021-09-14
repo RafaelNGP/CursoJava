fd2fa9416915eb47341bdd7b85a06f9d5d1d1716
rafael ferreira <rafaelferreira.dev3@gmail.com>
2021-09-14 15:19:18 -0300

    TESTE

--- src/udemy/proprios/ParaOPi.java
+++ src/udemy/proprios/ParaOPi.java
@@ -5,6 +5,7 @@ public class ParaOPi {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
             System.out.println("9x" + i + " = " + 9*i);
+            System.out.println("TESTE");
         }
     }
 }
